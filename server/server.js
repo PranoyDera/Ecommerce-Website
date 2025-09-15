b1be468cc4be8a8d07d7e76ecf549e406e93a379
Pranoy Dera <pranoy.dera@nzminds.com>
2025-09-15 14:10:34 +0530

    solved resend API key problem

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./Routes/authRoutes.js";
@@ -11,7 +12,6 @@ import paymentRoutes from "./Routes/paymentRoutes.js";
 import searchRoutes from "./Routes/searchRoutes.js";
 import feedbackRoutes from "./Routes/feedbackRoutes.js";
 
-dotenv.config();
 connectDB();
 
 const app = express();
--- server/utils/sendEmails.js
+++ server/utils/sendEmails.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_cm57XCi7_2ijvToDFjQeF7fVZy4Gm6Bvp");
 
 export const sendEmail = async (to, subject, text) => {
   try {
